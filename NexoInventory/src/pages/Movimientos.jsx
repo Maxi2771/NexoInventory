968c2992459c626717bb16171683b324eeac0d02
Gino Mazzaglia <mazzagliagino@gmail.com>
2025-09-11 16:48:04 -0300

    refactor Movimientos.jsx para incluir tabla y barra de búsqueda, actualiza la estructura de la interfaz

--- NexoInventory/src/pages/Movimientos.jsx
+++ NexoInventory/src/pages/Movimientos.jsx
@@ -1,5 +1,37 @@
+import DropdownButton from "../components/DropdownButton ";
+import SearchBar from "../components/SearchBar";
+import Table from "../components/Table";
+
 function Movimientos() {
-    return <h1 className="text-2xl p-4">📈 Bienvenido a la página de Movimientos</h1>;
+
+    const productColumns = [
+        { header: "ID", accessor: "id" },
+        { header: "Fecha y hora", accessor: "fecha y hora" },
+        { header: "Producto", accessor: "producto" },
+        { header: "Cantidad", accessor: "cantidad" },
+        { header: "Usuario", accessor: "usuario" },
+        { header: "Comentarios", accessor: "comentarios" },
+    ];
+
+    return (
+        <div className="text-white">
+            <div className="grid grid-cols-5 grid-rows-2 gap-8">
+                <div className="col-span-3 items-center">
+                    <h1>
+                        Gestion de Stock
+                    </h1>
+                </div>
+                <DropdownButton label="Hacer Ajuste" />
+                <DropdownButton label="Registrar Entrada" />
+                <div className="col-span-3">
+                    <SearchBar placeholder="Buscar productos o usuario" />
+                </div>
+                <DropdownButton label="Movimiento" />
+                <DropdownButton label="MM/DD/YYYY" />
+            </div>
+            <Table columns={productColumns} data={[]} />
+        </div>
+    );
 }
 
 export default Movimientos;

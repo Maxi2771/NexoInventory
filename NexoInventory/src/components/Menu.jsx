3c853bdf6c05fed0e9a685457196e97117775f56
Maxi2771 <maximogoitia277@gmail.com>
2025-09-21 12:47:07 -0300

    arreglo componentes menu, header y paginas movimiento y productos

--- NexoInventory/src/components/Menu.jsx
+++ NexoInventory/src/components/Menu.jsx
@@ -1,22 +1,11 @@
 import "../assets/style/input.css";
-import Dashboard from "../assets/img/icons/Dashboard.jsx";
-import Productos from "../assets/img/icons/Productos.jsx";
-import Movimientos from "../assets/img/icons/Movimientos.jsx"; 
-import Btn from "./Btn";
 
-function Menu() {
+
+function Menu({ children }) {
     return (
         <div className="w-xs h-screen bg-gray-800 text-white flex flex-col justify-center">
             <div className="flex flex-col space-y-2 w-xs justify-center items-center">
-                <Btn to="/" icon={Dashboard}>
-                    Dashboard
-                </Btn>
-                <Btn to="/productos" icon={Productos}>
-                    Productos
-                </Btn>
-                <Btn to="/movimientos" icon={Movimientos}>
-                    Movimientos
-                </Btn>
+                {children}
             </div>
         </div>
     );
--- NexoInventory/src/pages/Movimientos.jsx
+++ NexoInventory/src/pages/Movimientos.jsx
@@ -1,35 +1,56 @@
 import DropdownButton from "../components/DropdownButton ";
 import SearchBar from "../components/SearchBar";
 import Table from "../components/Table";
+import Header from "../components/header";
+import DownArrow from "../assets/img/icons/DownArrow";
+import Menu from "../components/Menu";
+import DashboardI from "../assets/img/icons/DashboardI.jsx";
+import ProductosI from "../assets/img/icons/ProductosI.jsx";
+import MovimientosI from "../assets/img/icons/MovimientosI.jsx"; 
+import Btn from "../components/Btn";
 
 function Movimientos() {
 
-    const productColumns = [
+    const movimientoColumns = [
         { header: "ID", accessor: "id" },
-        { header: "Fecha y hora", accessor: "fecha y hora" },
+        { header: "Fecha y hora", accessor: "fecha" },
         { header: "Producto", accessor: "producto" },
         { header: "Cantidad", accessor: "cantidad" },
         { header: "Usuario", accessor: "usuario" },
         { header: "Comentarios", accessor: "comentarios" },
     ];
 
+    const movimientoData = [
+        { id: '12345', fecha: '2023-10-01 10:00', producto: 'Auriculares Inalámbricos', cantidad: 10, usuario: 'admin', comentarios: 'Entrada de stock' },
+        { id: '54353', fecha: '2023-10-02 14:30', producto: 'Smartphone Galaxy X', cantidad: -5, usuario: 'user1', comentarios: 'Venta realizada' },
+        { id: '57567', fecha: '2023-10-03 09:15', producto: 'Laptop UltraBook Pro', cantidad: 3, usuario: 'admin', comentarios: 'Devolución de cliente' },
+    ];
+
     return (
-        <div className="text-white">
-            <div className="grid grid-cols-5 grid-rows-2 gap-8">
-                <div className="col-span-3 items-center">
-                    <h1>
-                        Gestion de Stock
-                    </h1>
+        <div className="w-full min-h-screen flex flex-row font-sans fixed">
+            <Menu>
+                <Btn to="/dashboard" icon={DashboardI}>
+                    Dashboard
+                </Btn>
+                <Btn to="/productos" icon={ProductosI}>
+                    Productos
+                </Btn>
+                <Btn to="/movimientos" icon={MovimientosI}>
+                    Movimientos
+                </Btn>
+            </Menu>
+            <div className="flex text-white flex-col w-full p-4">
+                <Header title="Movimientos">
+                    <SearchBar placeholder="Buscar por nombre de proveedor..." />
+                    <DropdownButton label="Agregar producto" icon={null} />
+                    <DropdownButton label="Importar|Exportar" icon={null} />
+                    <DropdownButton label="Ordenar por" icon={DownArrow}/>
+                    <DropdownButton label="Categoría" icon={DownArrow} />
+                </Header>
+                <div className="bg-gray-900 p-8 min-h-screen">
+                    <Table columns={movimientoColumns} data={movimientoData} />
                 </div>
-                <DropdownButton label="Hacer Ajuste" />
-                <DropdownButton label="Registrar Entrada" />
-                <div className="col-span-3">
-                    <SearchBar placeholder="Buscar productos o usuario" />
-                </div>
-                <DropdownButton label="Movimiento" />
-                <DropdownButton label="MM/DD/YYYY" />
             </div>
-            <Table columns={productColumns} data={[]} />
         </div>
     );
 }

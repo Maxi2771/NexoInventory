d43801a9032f1cf4fdef36815df14fcbbbe68bbe
Maxi2771 <maximogoitia277@gmail.com>
2025-09-24 00:29:25 -0300

    Uso para github pages

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://Maxi2771.github.io/NexoInventory/',
+  build: {
+    outDir: 'docs'
+  }
 })

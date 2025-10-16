d2213f6ebbf2d890bb448f56ee812c85699bf7cf
Maxi2771 <maximogoitia277@gmail.com>
2025-10-16 11:22:45 -0300

    arreglo para despliegue en netlfy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://Maxi2771.github.io/NexoInventory/',
+  //base: 'https://Maxi2771.github.io/NexoInventory/',
   build: {
     outDir: 'docs'
   }
